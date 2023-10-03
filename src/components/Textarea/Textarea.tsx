4c3afeae552c43987672bb93211995511b147416
Nick Kupriyanov <nickkupriyanov@MacBook-Pro-Nick.local>
2023-10-04 00:37:53 +0300

    fixed textarea.v4

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FC, useEffect, useRef, useState } from 'react';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import './Textarea.css';
 
 type TextareaProps = {
